863c275417f663aebf33f0c6e800471346f2ee3e
Marcin Tolysz <tolysz@gmail.com>
2016-09-07 22:25:54 +0100

    keter

---
+++ bundle/keter.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+OLD=$PWD
+
+rm -rf static
+cd ..
+
+
+./build-7.10.3.sh
+upx-ucl project-backend-exe
+
+cp project-backend-exe $OLD
+cp -r static $OLD 
+cd $OLD 
+
+tar -zcvpf spock.keter config/keter.yaml project-backend-exe static
+scp spock.keter spock8.kio.sx:keter
